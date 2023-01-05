55eed2a886e7418618577b4b0577cede5529d870
r3g_5z <june@girlboss.ceo>
2023-01-04 22:51:56 -0500

    javascript stuffs (im not a js dev but it works on my instance)
    also fix the licences lol sorry i was tired
    
    Signed-off-by: r3g_5z <june@girlboss.ceo>

--- public/js/hlsPlayback.js
+++ public/js/hlsPlayback.js
@@ -1,5 +1,3 @@
-// @license http://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
-// @source  https://github.com/video-dev/hls.js
-// @version v1.2.9
-function playVideo(e){const t=e.parentElement.querySelector("video"),a=t.getAttribute("data-url");if(t.setAttribute("controls",""),e.style.display="none",Hls.isSupported()){var l=new Hls({autoStartLoad:!1});l.loadSource(a),l.attachMedia(t),l.on(Hls.Events.MANIFEST_PARSED,(function(){l.loadLevel=l.levels.length-1,l.startLoad(),t.play()}))}else t.canPlayType("application/vnd.apple.mpegurl")&&(t.src=a,t.addEventListener("canplay",(function(){t.play()})))}
+// @license http://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
+function playVideo(e){const t=e.parentElement.querySelector("video"),a=t.getAttribute("data-url");if(t.setAttribute("controls",""),e.style.display="none",Hls.isSupported()){let e=new Hls({autoStartLoad:!1});e.loadSource(a),e.attachMedia(t),e.on(Hls.Events.MANIFEST_PARSED,(function(){e.loadLevel=e.levels.length-1,e.startLoad(),t.play()}))}else t.canPlayType("application/vnd.apple.mpegurl")&&(t.src=a,t.addEventListener("canplay",(function(){t.play()})))}
 // @license-end
